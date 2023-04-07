96f26fc7cce51f09176447ca1d043afbc739c83d
Marie P <map40j@gmail.com>
2023-04-06 22:23:14 -0700

    started working on the projects section

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -3,32 +3,27 @@ import Navbar from './navbar';
 import Hheventsweoffer from '../../assets/gif/hheventsweoffer.gif';
 import HootenannyHero from '../../assets/gif/HootenannyHero.gif';
 import HootenannyHero2
- from '../../assets/images/HootenannyHero2.gif';
-import Fig2 from '../../assets/images/fig2.png';
-import Fig3 from '../../assets/images/fig3.png';
-import Fig4 from '../../assets/images/fig4.png';
-import Fig5 from '../../assets/images/fig5.png';
-import Fig6 from '../../assets/images/fig6.png';
-import B1 from '../../assets/images/b1.png';
-import B2 from '../../assets/images/b2.png';
-import B3 from '../../assets/images/b3.png';
-import B4 from '../../assets/images/b4.png';
-import B5 from '../../assets/images/b5.png';
+ from '../../assets/gif/HootenannyHero2.gif';
+import MERNBookSearchSS from '../../assets/images/projects/MERNBookSearchSS.png';
+import Login from '../../assets/images/projects/Login.png';
+import logout from '../../assets/images/projects/logout.png';
+import Fig4 from '../../assets/images/UxUi/fig4.png';
+import Fig5 from '../../assets/images/UxUi/fig5.png';
+import Fig6 from '../../assets/images/UxUi/fig6.png';
+//import B1 from '../../assets/images/UxUi/b1.png';
+import B2 from '../../assets/images/UxUi/b2.png';
+import B3 from '../../assets/images/UxUi/b3.png';
+import B4 from '../../assets/images/UxUi/b4.png';
+import B5 from '../../assets/images/UxUi/b5.png';
 
 import Footer from './footer'
-import '../../assets/css/uxUi.css';
+import '../../assets/css/projects.css';
 
 const UxUi = () => {
     const [enlargedImg, setEnlargedImg] = useState(null);
   
     // state to show/hide the modal
     const [showModal, setShowModal] = useState(false); 
-  
-    // function to handle image click and set enlarged image state
-    const handleImageClick = (imgsrc) => {
-      setEnlargedImg(imgsrc);
-      setShowModal(true);
-    };
   
     // function to close the modal and reset enlarged image state
     const handleCloseModal = () => {
@@ -40,98 +35,86 @@ const UxUi = () => {
     <div>
     <Navbar />
     <h1>My Recent Projects</h1>
-    <p>Hootenany Hero</p>
+   
+     <h2>Hootenany Hero</h2>   
     
     <div className="flex-container">
-      <div className="flex-item">
-        <img src={Hheventsweoffer} alt="A1" className="ux-ui-image"
-      />
+          <div className="flex-item">
+        <img src={Hheventsweoffer} alt="Hheventsweoffer gif" className="ux-ui-image fig-size"/>
       </div>
       <div className="flex-item">
-        <img src={HootenannyHero} alt="A2" className="ux-ui-image"
-       onClick={() => handleImageClick("../../assets/images/A2.png")}  />
+        <img src={HootenannyHero} alt="HootenannyHero gif" className="ux-ui-image fig-size"/>
       </div>
-
+  
       <div className="flex-item">
-        <img src={HootenannyHero} alt="A2" className="ux-ui-image"
-       onClick={() => handleImageClick("../../assets/images/A2.png")}  />
+        <img src={HootenannyHero2} alt="HootenannyHero gif" className="ux-ui-image fig-size"/>
       </div>
-      
-      <h2>Example of Figma</h2>
+      </div>
+      <h2>MERN Book Search</h2>
       <div className="flex-container">
         <div className="flex-itemw">
           <img
-            src={Fig1}
-            alt="Fig1"
-            className="ux-ui-image fig-size"
-       onClick={() => handleImageClick("../../assets/images/fig1.png")}  />
+            src={MERNBookSearchSS}
+            alt="MERNBookSearchSS"
+            className="ux-ui-image fig-size"/>
         </div>
         <div className="flex-itemw">
           <img
-            src={Fig2}
-            alt="Fig2"
-            className="ux-ui-image fig-size"
-            onClick={() => handleImageClick("../../assets/images/fig2.png")}  />
+            src={Login}
+            alt="login"
+            className="ux-ui-image fig-size"/>
         </div>
         <div className="flex-itemw">
           <img
-            src={Fig3}
-            alt="Fig3"
-            className="ux-ui-image fig-size"
-       onClick={() => handleImageClick("../../assets/images/fig3.png")}  />
+            src={logout}
+            alt="logout"
+            className="ux-ui-image fig-size"/>
         </div>
-        <div className="flex-item">
+        </div>
+        <h2>Final Output for both</h2>
+        <div className="flex-itemw">
           <img
             src={Fig4}
             alt="Fig4"
-            className="ux-ui-image"
-            onClick={() => handleImageClick("../../assets/images/fig4.png")}  />
+            className="ux-ui-image fig-size"/>
         </div>
-        <div className="flex-item">
+        <div className="flex-itemw">
           <img
             src={Fig5}
             alt="Fig5"
-            className="ux-ui-image"
-            onClick={() => handleImageClick("../../assets/images/fig5.png")}  />
+            className="ux-ui-image fig-size"/>
         </div>
-        <div className="flex-item">
+        <div className="flex-itemw">
           <img
             src={Fig6}
             alt="Fig6"
-            className="ux-ui-image"
-            onClick={() => handleImageClick("../../assets/images/fig6.png")}  />
+            className="ux-ui-image fig-size"/>
         </div>
-      </div>
+      
   
     <h2>Final Output for both</h2>
-    <div className="flex-container">
-      <div className="ux-ui-image"
-       onClick={() => handleImageClick("../../assets/images/b1.png")}  />
+
+      
+      <div className="flex-item">
+        <img src={B2} alt="b2" className="ux-ui-image fig-size"/>
+      </div>
+      <div className="flex-itemw">
+        <img src={B3} alt="b3"className="ux-ui-image fig-size"/>
+      </div>
+      <div className="flex-itemw">
+        <img src={B4} alt="b4" className="ux-ui-image fig-size"/>
       </div>
       <div className="flex-item">
-        <img src={B2} alt="b2" className="ux-ui-image"
-       onClick={() => handleImageClick("../../assets/images/b2.png")}  />
+        <img src={B5} alt="b5" className="ux-ui-image fig-size"/>
       </div>
-      <div className="flex-item">
-        <img src={B3} alt="b3"className="ux-ui-image"
-       onClick={() => handleImageClick("../../assets/images/b3.png")}  />
-      </div>
-      <div className="flex-item">
-        <img src={B4} alt="b4" className="ux-ui-image"
-       onClick={() => handleImageClick("../../assets/images/b4.png")}  />
-      </div>
-      <div className="flex-item">
-        <img src={B5} alt="b5" className="ux-ui-image"
-       onClick={() => handleImageClick("../../assets/images/b5.png")}  />
-      </div>
-    </div>
+  
 
    {/* Modal for enlarged image */}
    {showModal && (
         <div className="modal">
           <div className="modal-content">
             <span className="close" onClick={handleCloseModal}>&times;</span>
-            <img src={enlargedImg} alt="Enlarged image" className="enlarged-image" />
+            <img src={enlargedImg} alt="Enlarged" className="enlarged-image" />
           </div>
         </div>
       )}
--- src/components/pages/uxUi.js
+++ src/components/pages/uxUi.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 import Navbar from './navbar';
-import A1 from '../../assets/images/A1.png';
-import A2 from '../../assets/images/A2.png';
+import A1 from '../../assets/images/UxUi/A1.png';
+import A2 from '../../assets/images/UxUi/A2.png';
 import Fig1
- from '../../assets/images/fig1.png';
-import Fig2 from '../../assets/images/fig2.png';
-import Fig3 from '../../assets/images/fig3.png';
-import Fig4 from '../../assets/images/fig4.png';
-import Fig5 from '../../assets/images/fig5.png';
-import Fig6 from '../../assets/images/fig6.png';
-import B1 from '../../assets/images/b1.png';
-import B2 from '../../assets/images/b2.png';
-import B3 from '../../assets/images/b3.png';
-import B4 from '../../assets/images/b4.png';
-import B5 from '../../assets/images/b5.png';
+ from '../../assets/images/UxUi/fig1.png';
+import Fig2 from '../../assets/images/UxUi/fig2.png';
+import Fig3 from '../../assets/images/UxUi/fig3.png';
+import Fig4 from '../../assets/images/UxUi/fig4.png';
+import Fig5 from '../../assets/images/UxUi/fig5.png';
+import Fig6 from '../../assets/images/UxUi/fig6.png';
+import B1 from '../../assets/images/UxUi/b1.png';
+import B2 from '../../assets/images/UxUi/b2.png';
+import B3 from '../../assets/images/UxUi/b3.png';
+import B4 from '../../assets/images/UxUi/b4.png';
+import B5 from '../../assets/images/UxUi/b5.png';
 
 import Footer from './footer'
 import '../../assets/css/uxUi.css';
@@ -24,11 +24,6 @@ const UxUi = () => {
     // state to show/hide the modal
     const [showModal, setShowModal] = useState(false); 
   
-    // function to handle image click and set enlarged image state
-    const handleImageClick = (imgsrc) => {
-      setEnlargedImg(imgsrc);
-      setShowModal(true);
-    };
   
     // function to close the modal and reset enlarged image state
     const handleCloseModal = () => {
@@ -44,11 +39,11 @@ const UxUi = () => {
     <div className="flex-container">
       <div className="flex-item">
         <img src={A1} alt="A1" className="ux-ui-image"
-       onClick={() => handleImageClick("../../assets/images/A1.png")}  />
+       />
       </div>
       <div className="flex-item">
         <img src={A2} alt="A2" className="ux-ui-image"
-       onClick={() => handleImageClick("../../assets/images/A2.png")}  />
+       />
       </div>
       
       <h2>Example of Figma</h2>
@@ -58,65 +53,67 @@ const UxUi = () => {
             src={Fig1}
             alt="Fig1"
             className="ux-ui-image fig-size"
-       onClick={() => handleImageClick("../../assets/images/fig1.png")}  />
+        />
         </div>
         <div className="flex-itemw">
           <img
             src={Fig2}
             alt="Fig2"
             className="ux-ui-image fig-size"
-            onClick={() => handleImageClick("../../assets/images/fig2.png")}  />
+             />
         </div>
         <div className="flex-itemw">
           <img
             src={Fig3}
             alt="Fig3"
             className="ux-ui-image fig-size"
-       onClick={() => handleImageClick("../../assets/images/fig3.png")}  />
+        />
         </div>
         <div className="flex-item">
           <img
             src={Fig4}
             alt="Fig4"
             className="ux-ui-image"
-            onClick={() => handleImageClick("../../assets/images/fig4.png")}  />
+             />
         </div>
         <div className="flex-item">
           <img
             src={Fig5}
             alt="Fig5"
             className="ux-ui-image"
-            onClick={() => handleImageClick("../../assets/images/fig5.png")}  />
+            />
         </div>
         <div className="flex-item">
           <img
             src={Fig6}
             alt="Fig6"
             className="ux-ui-image"
-            onClick={() => handleImageClick("../../assets/images/fig6.png")}  />
+            />
         </div>
       </div>
   
     <h2>Final Output for both</h2>
     <div className="flex-container">
-      <div className="ux-ui-image"
-       onClick={() => handleImageClick("../../assets/images/b1.png")}  />
+       <div className="flex-item">
+        <img src={B1} alt="b1" className="ux-ui-image"
+      />
+      </div>
       </div>
       <div className="flex-item">
         <img src={B2} alt="b2" className="ux-ui-image"
-       onClick={() => handleImageClick("../../assets/images/b2.png")}  />
+      />
       </div>
       <div className="flex-item">
         <img src={B3} alt="b3"className="ux-ui-image"
-       onClick={() => handleImageClick("../../assets/images/b3.png")}  />
+      />
       </div>
       <div className="flex-item">
         <img src={B4} alt="b4" className="ux-ui-image"
-       onClick={() => handleImageClick("../../assets/images/b4.png")}  />
+       />
       </div>
       <div className="flex-item">
         <img src={B5} alt="b5" className="ux-ui-image"
-       onClick={() => handleImageClick("../../assets/images/b5.png")}  />
+       />
       </div>
     </div>
 
@@ -125,7 +122,7 @@ const UxUi = () => {
         <div className="modal">
           <div className="modal-content">
             <span className="close" onClick={handleCloseModal}>&times;</span>
-            <img src={enlargedImg} alt="Enlarged image" className="enlarged-image" />
+            <img src={enlargedImg} alt="Enlarged" className="enlarged-image" />
           </div>
         </div>
       )}
